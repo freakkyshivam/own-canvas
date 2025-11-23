3862f99ba3c881832c1e79891237d16dd065408e
Shivam Chaudhary <skc722768@gmail.com>
2025-11-23 13:11:21 +0530

    deployement fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,34 @@
-import { useEffect } from 'react'
-import { Tldraw, toRichText, type Editor } from 'tldraw'
+import { Editor, Tldraw, toRichText } from 'tldraw'
 import 'tldraw/tldraw.css'
 
 export default function App() {
-  useEffect(() => {
-    console.log('🔵 App mounted')
-
-    return () => {
-      console.log('🔴 App UNMOUNTED')
-    }
-  }, [])
-
-  const handleMount = (editor: Editor) => {
-    console.log('🟢 Tldraw mounted')
+	const handleMount = (editor:Editor) => {
 
     editor.user.updateUserPreferences({
       colorScheme: 'dark',
     })
 
-    editor.createShape({
-      type: 'text',
-      x: 200,
-      y: 200,
-      props: {
-        richText: toRichText('Shivam Chaudhary!'),
-      },
-    })
-  }
+		editor.createShape({
+			type: 'text',
+			x: 200,
+			y: 200,
+			props: {
+				richText: toRichText('Shivam Chaudhary!'),
+			},
+		})
 
-  return (
-    <div style={{ position: 'fixed', inset: 0, background: 'black' }}>
-      <div
-        style={{
-          position: 'absolute',
-          zIndex: 9999,
-          color: 'white',
-          top: 8,
-          left: 8,
-        }}
-      >
-        APP RUNNING
-      </div>
-      <Tldraw onMount={handleMount} />
-    </div>
-  )
+		editor.selectAll()
+
+		  editor.zoomToSelection({
+      animation: { duration: 5000 },
+    })
+  
+		 
+	}
+
+	return (
+		<div style={{ position: 'fixed', inset: 0 }}>
+			<Tldraw  onMount={handleMount} />
+		</div>
+	)
 }
