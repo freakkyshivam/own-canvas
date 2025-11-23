e04c075f377e552f65863f68587e2fdd01039587
Shivam Chaudhary <skc722768@gmail.com>
2025-11-23 12:43:54 +0530

    deployement fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['tldraw'],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/tldraw/, /node_modules/],
-    },
-  },
 })
