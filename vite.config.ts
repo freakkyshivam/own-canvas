2bdcf94bf31672fe732b5ac3c05be5ab01ba6d94
Shivam Chaudhary <skc722768@gmail.com>
2025-11-23 13:15:31 +0530

    change version

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: [
+      '@tldraw/tldraw',
+      '@tldraw/utils',
+      '@tldraw/state',
+      '@tldraw/store',
+      '@tldraw/editor',
+      '@tldraw/ui',
+    ],
+  },
 })
