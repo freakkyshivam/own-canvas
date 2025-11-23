ccd96006a9d213e7d19515118c7d869e9839859b
Shivam Chaudhary <skc722768@gmail.com>
2025-11-23 12:18:44 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { Editor, Tldraw, toRichText } from 'tldraw'
+import 'tldraw/tldraw.css'
+
+export default function App() {
+	const handleMount = (editor:Editor) => {
+
+    editor.user.updateUserPreferences({
+      colorScheme: 'dark',
+    })
+
+		editor.createShape({
+			type: 'text',
+			x: 200,
+			y: 200,
+			props: {
+				richText: toRichText('Shivam Chaudhary!'),
+			},
+		})
+
+		editor.selectAll()
+
+		editor.zoomToSelection({
+			animation: { duration: 5000 },
+		})
+	}
+
+	return (
+		<div style={{ position: 'fixed', inset: 0 }}>
+			<Tldraw  onMount={handleMount} />
+		</div>
+	)
+}
