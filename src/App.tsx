0cd6a836045d6d95dc53c6db4739e4b9ae20e840
Shivam Chaudhary <skc722768@gmail.com>
2025-12-06 10:15:13 +0530

    xcvbnm

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ export default function App() {
 
 	return (
 		<div style={{ position: 'fixed', inset: 0 }}>
-			<Tldraw licenseKey='tldraw-2030-11-24/WyJSLXZ2VVFnYyIsWyIqLm93bi1jYW52YXMudmVyY2VsLmFwcCJdLDksIjIwMzAtMTEtMjQiXQ.T4FxYwWrTxadaHz8rZJHPO9VX2INRsH8wNQhu8qBtvF1BXbr2uYGW9GBC/GfCZjzZsl5v+oqorNG7YVjxd+dVg'  onMount={handleMount}  />
+			<Tldraw  onMount={handleMount} licenseKey='tldraw-2030-11-24/WyJSLXZ2VVFnYyIsWyIqLm93bi1jYW52YXMudmVyY2VsLmFwcCJdLDksIjIwMzAtMTEtMjQiXQ.T4FxYwWrTxadaHz8rZJHPO9VX2INRsH8wNQhu8qBtvF1BXbr2uYGW9GBC/GfCZjzZsl5v+oqorNG7YVjxd+dVg'  />
 		</div>
 	)
 }
