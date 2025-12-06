a7038e5c3100b624bfb533cd06c105156dd1def7
Shivam Chaudhary <skc722768@gmail.com>
2025-12-06 10:19:16 +0530

    v bn

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ export default function App() {
 
 	return (
 		<div style={{ position: 'fixed', inset: 0 }}>
-			<Tldraw  onMount={handleMount} licenseKey='tldraw-2030-11-24/WyJSLXZ2VVFnYyIsWyIqLm93bi1jYW52YXMudmVyY2VsLmFwcCJdLDksIjIwMzAtMTEtMjQiXQ.T4FxYwWrTxadaHz8rZJHPO9VX2INRsH8wNQhu8qBtvF1BXbr2uYGW9GBC/GfCZjzZsl5v+oqorNG7YVjxd+dVg'  />
+			<Tldraw  onMount={handleMount} licenseKey={import.meta.env.VITE_TLDRAW_LICENSE_KEY}  />
 		</div>
 	)
 }
